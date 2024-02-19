4d37294db5dc6352ed70f19f544bc44585979a15
anass zeroual <anasszeroual09@gmail.com>
2024-02-19 14:52:06 +0100

    pushiki

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import NavBar from './component/ui/NavBar';
 
 function App() {
   return (
     <div className="App">
-    
+      <NavBar />
     </div>
   );
 }
---
+++ src/services/api.js
@@ -0,0 +1,5 @@
+
+ const api = 'http://10.236.150.63:8880/api';
+ export default api;
+
+
